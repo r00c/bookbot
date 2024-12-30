0672cdf88349a9ced0013434c67a721b82b99c52
r00c <r00elung@gmail.com>
2024-12-30 17:10:47 +0800

    update word_count fucntion

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def main():
-	print(read_file("books/frankenstein.txt"))
-
+	article = read_file("books/frankenstein.txt")
+	print(article)
+	print(word_count(article))
 
 def read_file(path):
 	with open(path) as f:
 		return f.read()
+
+def word_count(article):
+	word = len(article.split())
+	return word
 
 main()
