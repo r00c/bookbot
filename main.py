6addf2f5806d752995d6c119187112a977c5e11e
r00c <r00elung@gmail.com>
2024-12-30 19:14:22 +0800

    rewrite the word counting function and finish the char counting function

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import string
+
 def main():
-	article = read_file("books/frankenstein.txt")
-	print(article)
-	print(word_count(article))
+    print(read_char("books/frankenstein.txt"))
 
-def read_file(path):
-	with open(path) as f:
-		return f.read()
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+    
+def read_word(filename):
+    words = read_file(filename).lower().split()
+    return words
 
-def word_count(article):
-	word = len(article.split())
-	return word
+def count_words(filename):
+    return len(read_word(filename))
 
+def read_char(filename):
+    count_char = {}
+    for char in string.ascii_lowercase:
+        count_char[char] = 0
+
+    for word in read_word(filename):
+        for char in word:
+            if char.isalpha():
+                count_char[char] += 1
+
+    return count_char
+    
 main()
