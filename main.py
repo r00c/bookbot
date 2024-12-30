e00c75b88a3848b7c86ef61e0812a90dec2f7d67
r00c <r00elung@gmail.com>
2024-12-30 19:27:01 +0800

    add print_format function

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 import string
 
 def main():
-    print(read_char("books/frankenstein.txt"))
+    print_format("books/frankenstein.txt")
+
 
 def read_file(filename):
     with open(filename, 'r') as f:
         return f.read()
     
+
 def read_word(filename):
     words = read_file(filename).lower().split()
     return words
 
+
 def count_words(filename):
     return len(read_word(filename))
 
-def read_char(filename):
-    count_char = {}
+
+def count_chars(filename):
+    chars = {}
     for char in string.ascii_lowercase:
-        count_char[char] = 0
+        chars[char] = 0
 
     for word in read_word(filename):
         for char in word:
             if char.isalpha():
-                count_char[char] += 1
+                chars[char] += 1
 
-    return count_char
+    return chars
+
+def print_format(filename):
+	print(f"--- Begin report of {filename} ---")
+	print(f"{count_words(filename)} words found in the document")
+	print("\n")
+	for char, count in count_chars(filename).items():
+		print(f"The {char} character was found {count} times")
+	print("--- End report ---")
     
 main()
